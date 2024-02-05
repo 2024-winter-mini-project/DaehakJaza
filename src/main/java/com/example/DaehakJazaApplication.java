688565b358ebf1f08aaa705690a836245fee239a
patchpark <kevinminsang99@gmail.com>
2024-02-05 23:50:51 +0900

    basic-settings

---
+++ src/main/java/com/example/DaehakJazaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaehakJazaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaehakJazaApplication.class, args);
+	}
+
+}
