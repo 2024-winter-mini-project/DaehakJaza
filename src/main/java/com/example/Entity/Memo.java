ba2672ff83981b6cffd942f171572b4097717770
patchpark <kevinminsang99@gmail.com>
2024-02-13 15:07:52 +0900

    mysql 연동 기본 세팅

---
+++ src/main/java/com/example/Entity/Memo.java
@@ -0,0 +1,20 @@
+package com.example.Entity;
+
+import lombok.*;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "test")
+@Data
+@Builder
+@AllArgsConstructor // @Builder 를 이용하기 위해서 항상 같이 처리해야 컴파일 에러가 발생하지 않는다
+@NoArgsConstructor // @Builder 를 이용하기 위해서 항상 같이 처리해야 컴파일 에러가 발생하지 않는다
+public class Memo {
+
+    @Id // @Entity 가 붙은 클래스는 PK에 해당하는 특정필드를 @Id로 지정해야 한다
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long mno;
+
+    @Column
+    private String memoText;
+}
